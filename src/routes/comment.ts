f68a8ab2ad13f98b3618baa462abcbad3e97e727
ulxsth <ulxsth@gmail.com>
2023-11-27 11:47:14 +0900

    Fix comment edit link in post show view

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,4 +1,5 @@
 import { Comment } from "@/models/comment";
+import { Post } from "@/models/post";
 import express from "express";
 
 export const commentRouter = express.Router();
@@ -13,7 +14,7 @@ commentRouter.post("/:postId",
     }
 
     const { postId } = req.params;
-    if (postId === undefined || Comment.find(Number(postId)) === undefined) {
+    if (postId === undefined || Post.find(Number(postId)) === undefined) {
       res.redirect("/404");
       return;
     }
@@ -25,3 +26,67 @@ commentRouter.post("/:postId",
     req.dialogMessage?.setMessage("Comment successfully created");
     res.redirect(`/posts/${postId}`);
   });
+
+commentRouter.get("/:commentId/edit", async (req, res, next) => {
+  const currentUserId = req.authentication?.currentUserId;
+
+  if (currentUserId === undefined) {
+    res.redirect("/401");
+    return;
+  }
+
+  const { commentId } = req.params;
+  if (commentId === undefined || Comment.find(Number(commentId)) === undefined) {
+    res.redirect("/404");
+    return;
+  }
+
+  const comment = await Comment.find(Number(commentId));
+  if (comment === undefined || comment.userId !== currentUserId) {
+    res.redirect("/404");
+    return;
+  }
+
+  if (comment.userId !== currentUserId) {
+    res.redirect("/403");
+    return;
+  }
+
+  res.render("comments/edit", {
+    comment,
+    errors: [],
+  });
+});
+
+commentRouter.patch("/:commentId", async (req, res, next) => {
+  const currentUserId = req.authentication?.currentUserId;
+
+  if (currentUserId === undefined) {
+    res.redirect("/401");
+    return;
+  }
+
+  const { commentId } = req.params;
+  if (commentId === undefined || Comment.find(Number(commentId)) === undefined) {
+    res.redirect("/404");
+    return;
+  }
+
+  const comment = await Comment.find(Number(commentId));
+  if (comment === undefined || comment.userId !== currentUserId) {
+    res.redirect("/404");
+    return;
+  }
+
+  if (comment.userId !== currentUserId) {
+    res.redirect("/403");
+    return;
+  }
+
+  const { content } = req.body;
+  comment.content = content;
+  comment.save();
+
+  req.dialogMessage?.setMessage("Comment successfully updated");
+  res.redirect(`/posts/${comment.postId}`);
+});
