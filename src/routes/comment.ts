a059a0d3007bdee9a0565ad84ed0f644796bd762
ulxsth <ulxsth@gmail.com>
2023-11-27 09:47:36 +0900

    Add comment creation functionality

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,3 +1,15 @@
+import { Comment } from "@/models/comment";
 import express from "express";
 
 export const commentRouter = express.Router();
+
+commentRouter.post("/", (req, res, next) => {
+  const currentUserId = req.authentication?.currentUserId;
+  if (currentUserId === undefined) {
+    return next(new Error("Invalid error: currentUserId is undefined."));
+  }
+  const { postId, content } = req.body;
+  const comment = new Comment(currentUserId, postId, content);
+  comment.save();
+  res.redirect(`/posts/${postId}`);
+});
